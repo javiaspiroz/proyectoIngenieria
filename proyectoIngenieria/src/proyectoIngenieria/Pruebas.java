d229cb69fffb8cfdaa50ed5489f6749b5081d60e
mariasol210 <58983798+mariasol210@users.noreply.github.com>
2020-05-14 16:44:49 -0500

    Una clase para hacer las pruebas unitarias con JUnit4

---
+++ proyectoIngenieria/src/proyectoIngenieria/Pruebas.java
@@ -0,0 +1,26 @@
+package proyectoIngenieria;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class Pruebas {
+	
+	Hospital hospital;
+	
+	@Before
+	public void inicializar() {
+		hospital = new Hospital ("pacientes.csv");
+	}
+	
+	@Test
+	public void pruebaExportarcsv() {
+		
+		hospital.exportar_csv("pacientes_pruebas.csv", 'P');
+		//File fichero = new File("pacientes_pruebas.csv");
+		assertTrue(new File("pacientes_pruebas.csv").exists());
+	}
+}
