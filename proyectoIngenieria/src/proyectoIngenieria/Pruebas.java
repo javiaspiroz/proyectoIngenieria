960d0aa57155648e0e72847b68b874e438deee7e
raduscrobota <raduscrobota@192.168.1.221>
2020-05-20 20:00:29 +0200

    prueba

--- proyectoIngenieria/src/proyectoIngenieria/Pruebas.java
+++ proyectoIngenieria/src/proyectoIngenieria/Pruebas.java
@@ -22,6 +22,6 @@ public class Pruebas {
 		hospital.exportar_csv("pacientes_pruebas.csv", 'P');
 		//File fichero = new File("pacientes_pruebas.csv");
 		assertTrue(new File("pacientes_pruebas.csv").exists());
-		System.out.println("Prueba Radu");
+		System.out.println("Prueba Radu1");
 	}
 }
